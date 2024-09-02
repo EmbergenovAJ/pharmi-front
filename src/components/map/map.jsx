93bb9136b0d63b564796c01179d28676e217681e
EmbergenovAJ <19ayda967198@gmail.com>
2024-09-02 19:37:00 +0500

    little refactor in footer

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -5,13 +5,14 @@ export default function Map() {
 
     return (
         // 41.2980499,69.2754893
+        ////// 41.354155,69.332057
         <div style={{ height: '500px' }}>
-            <MapContainer center={[41.2980499, 69.2754893]} zoom={18} scrollWheelZoom={false} style={{ height: '100%' }}>
+            <MapContainer center={[41.354155, 69.332057]} zoom={16} scrollWheelZoom={false} style={{ height: '100%' }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[41.2980499, 69.2754893]}>
+                <Marker position={[41.354155, 69.332057]}>
                     <Popup>
                         Toshkent farmatsevtika instituti
                     </Popup>
